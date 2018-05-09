ab16cbe223fa8f85c19fe0bbc1edc94fef1993f6
Alex Lerner <alernerdev@gmail.com>
2018-05-08 21:19:08 -0400

    developed contract deployment test script and first test

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -5,6 +5,28 @@ const Web3 = require('web3');
 // web3 takes a provider as an argument to talk to a specific network.
 const web3 = new Web3(ganache.provider());
 
+const {interface, bytecode} = require('../compile');
+
+let accounts;
+let inbox;
+beforeEach(async ()=>{
+    // get a list of all accounts
+    // the test network gives us a pre-made list of unlocked accounts
+    accounts = await web3.eth.getAccounts();
+   
+
+    // use one of the accounts to deploy the contract
+    // arguments contains params into contract constructor
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({data: bytecode, arguments:['Hi there']})
+        .send({from: accounts[0], gas:'1000000'})
+});
+
+describe('Inbox', () => {
+    it('deploys a contract', () => {
+        assert.ok(inbox.options.address);
+    });
+})
 
 /*
 class Car {
